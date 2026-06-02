722e373dfaa8b3182da5c421d16a581b21fe1bd0
Amit Niranjan <amit.niranjan@stikkmanux.com>
2026-06-02 11:41:28 +0530

    Remove transition

--- asian-cables-next-website/components/layout/WebsiteNavbar.tsx
+++ asian-cables-next-website/components/layout/WebsiteNavbar.tsx
@@ -119,7 +119,7 @@ const isHomePage = pathname === "/";
 
    
     <nav
-      className={`fixed top-0 right-0 left-0 z-50 transition-all duration-300 ${
+      className={`fixed top-0 right-0 left-0 z-50 transition-all ${
         isScrolled || showMegaMenu ? "bg-white shadow-sm" : "bg-transparent"
       }`}
     >
