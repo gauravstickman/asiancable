3e6bea377e1da769bc0786d3ab063630909d6b77
Amit Niranjan <amit.niranjan@stikkmanux.com>
2026-06-03 20:29:09 +0530

    menu logo fixed

--- asian-cables-next-website/utils/api.ts
+++ asian-cables-next-website/utils/api.ts
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000/api',
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://187.127.133.212:5000/api',
 });
 
-export const getBaseUrl = () => process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:5000';
+export const getBaseUrl = () => process.env.NEXT_PUBLIC_BASE_URL || 'http://187.127.133.212:5000';
 
 export default api;
