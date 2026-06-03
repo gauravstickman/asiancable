c28410d71004255c9c5c7bc07560cd796593ff50
Amit Niranjan <amit.niranjan@stikkmanux.com>
2026-06-04 01:24:46 +0530

    changes in menu and scroll

--- asian-cables-next-website/components/layout/MobileProductMenu.tsx
+++ asian-cables-next-website/components/layout/MobileProductMenu.tsx
@@ -13,13 +13,26 @@ export default function MobileProductMenu({
   industryGroups?: Record<string, any[]>;
   typeGroups?: Record<string, any[]>;
 }) {
-    useEffect(() => {
-    document.body.style.overflow = "hidden";
 
-    return () => {
-      document.body.style.overflow = "";
-    };
-  }, []);
+  useEffect(() => {
+  const scrollY = window.scrollY;
+
+  document.body.style.position = "fixed";
+  document.body.style.top = `-${scrollY}px`;
+  document.body.style.left = "0";
+  document.body.style.right = "0";
+  document.body.style.width = "100%";
+
+  return () => {
+    document.body.style.position = "";
+    document.body.style.top = "";
+    document.body.style.left = "";
+    document.body.style.right = "";
+    document.body.style.width = "";
+
+    window.scrollTo(0, scrollY);
+  };
+}, []);
 
   const [mainAccordion, setMainAccordion] =
     useState("industry");
