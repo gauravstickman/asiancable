4d5c3e23c097b3fbebcfb4797caf4975eacda53a
Amit Niranjan <amit.niranjan@stikkmanux.com>
2026-06-04 12:52:07 +0530

    changes in tsx

--- asian-cables-next-website/components/pages/LifeAtAsianCables/page.tsx
+++ asian-cables-next-website/components/pages/LifeAtAsianCables/page.tsx
@@ -2,14 +2,14 @@
 
 import Footer from "../../layout/Footer";
 import WebsiteNavbar from "../../layout/WebsiteNavbar";
-import TestimonialsSection from "./parts/TestimonialsSection";
-import CultureValuesSection from "./parts/CultureValuesSection";
-import DayInLifeSection from "./parts/DayInLifeSection";
-import ExperienceSection from "./parts/ExperienceSection";
-import Hero from "./parts/Hero";
-import LifeBeyondWorkSection from "./parts/LifeBeyondWorkSection";
-import OpenRolesSection from "./parts/OpenRolesSection";
-import WhyWorkSection from "./parts/Whyworksection";
+import TestimonialsSection from "../../pages/LifeAtAsianCables/parts/TestimonialsSection";
+import CultureValuesSection from "../../pages/LifeAtAsianCables/parts/CultureValuesSection";
+import DayInLifeSection from "../../pages/LifeAtAsianCables/parts/DayInLifeSection";
+import ExperienceSection from "../../pages/LifeAtAsianCables/parts/ExperienceSection";
+import Hero from "../../pages/LifeAtAsianCables/parts/Hero";
+import LifeBeyondWorkSection from "../../pages/LifeAtAsianCables/parts/LifeBeyondWorkSection";
+import OpenRolesSection from "../../pages/LifeAtAsianCables/parts/OpenRolesSection";
+import WhyWorkSection from "../../pages/LifeAtAsianCables/parts/Whyworksection";
 
 const Life = () => {
   return (
